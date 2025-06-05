0e2dd775c62b76e4699ede877ff7c5ddb3c2bdb1
AngelFlrz <101581224+AngelFlrz@users.noreply.github.com>
2025-06-05 17:32:54 -0500

    Update page.js

--- src/app/inventario/page.js
+++ src/app/inventario/page.js
@@ -9,7 +9,23 @@ import { app } from '../lib/firebase';
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'angelmflorez15@gmail.com', // Change to your recipient
+  from: 'pinturillojuanso@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
 
 export default function Inventario() {
   const router = useRouter();
